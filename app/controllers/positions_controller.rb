ff017e8bfa524b8c9fbdddaa5f6286d26a982c3a
AmiasYaska <amiasyaska@gmail.com>
2025-03-25 15:14:47 +0300

    changes

---
+++ app/controllers/positions_controller.rb
@@ -0,0 +1,10 @@
+class PositionsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def vote
+  end
+end
---
+++ test/controllers/positions_controller_test.rb
@@ -0,0 +1,18 @@
+require "test_helper"
+
+class PositionsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get positions_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get positions_show_url
+    assert_response :success
+  end
+
+  test "should get vote" do
+    get positions_vote_url
+    assert_response :success
+  end
+end
