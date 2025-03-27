7158e9fe6791bc6f1f70b5105d132ce2f760a947
AmiasYaska <amiasyaska@gmail.com>
2025-03-27 15:48:17 +0300

    changes

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,10 +1,19 @@
 class Candidate < ApplicationRecord
   belongs_to :position
   has_many :votes, dependent: :destroy
-
+  
   has_one_attached :photo
 
   validates :name, presence: true
 
-  validates :photo, size: { less_than: 5.megabytes, message: "must be less than 5MB" }
+  # validates :photo, presence: true
+  validates :photo, presence: true
+
+  private
+
+  # def photo_format
+  #   if photo.byte_size > 5.megabytes
+  #     errors.add(:photo, "must be less than 5MB")
+  #   end
+  # end
 end
